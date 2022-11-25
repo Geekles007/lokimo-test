c329423432fee32add0c092d87738dcbf47e9dce
Geekles <tondjilee@gmail.com>
2022-11-25 14:21:47 +0100

    responsive

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,7 +12,7 @@ type HomePageProps = {}
 const HomePage = ({}: HomePageProps) => {
     const {adverts} = useAppContext<IAdvertisement>();
 
-    return <div className={"flex gap-y-8 flex-col lg:grid lg:gap-8 lg:grid-cols-12 grid-rows-6 h-full"}>
+    return <div className={"flex gap-y-2 flex-col lg:grid lg:gap-8 lg:grid-cols-12 grid-rows-6 h-full"}>
         <div className="w-full lg:w-auto col-span-12 lg:col-span-4 row-span-1 lg:row-span-6 lg:h-full flex flex-col gap-8">
             <Filters />
             <Listing />
